7940b3f97648699621f996ef14e594606e84dff3
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-07-09 21:16:08 +0200

    all interaction added in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import logo from './logo.svg';
 import Form from './Form';
 import Section from './Section';
 import Container from "./Container";
@@ -7,13 +6,51 @@ import Container from "./Container";
 
 function App() {
 
-  const [amount, setAmount] = useState();
-  const [resultValue, setResultValue] = useState();
+  const eur = 4.55;
+    
+
+  const clearAmount = () =>{ 
+    setAmount(amount => amount = "")
+  }
+
+  const clearCurrency = () => {
+    setCurrency(currency => currency =  eur)
+  }
+
+  const [currency, setCurrency] = useState("");
+
+  const onSelectChange = ({target}) => setCurrency(target.value);
+
+  const [amount, setAmount] = useState("");
+
+  const onInputChange = ({target}) => setAmount(target.value);
+
+  const getResult = () => {
+    setResultValue(resultValue => resultValue = amount/currency )
+  };
+
+  const [resultValue, setResultValue] = useState("");
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    getResult();
+    clearAmount();
+    clearCurrency();
+    console.log(amount);
+    console.log(currency);
+    console.log(resultValue);
+    
+  }
 
   return (
     <Container >
-      <Form />
-      <Section />
+      <Form 
+      amount={amount} 
+      currency={currency}  
+      onFormSubmit={onFormSubmit} 
+      onInputChange={onInputChange}
+      onSelectChange={onSelectChange} />
+      <Section result={resultValue} />
     </ Container>
   );
 }
