2882d6e327dd8f6157827e28431ecbe1cf7202a5
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-07-09 21:12:36 +0200

    Form interaction added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,31 +1,44 @@
 import "./style.css";
 
-const Form = ({ amount, resultValue }) => {
-    const eur = 4.55;
+const Form = ({eur, amount, currency, onFormSubmit, onInputChange, onSelectChange }) => {
     const usd = 3.79;
     const chf = 4.13;
 
     return (
-        <form className="form js-form">
+        <form className="form"
+            onSubmit={onFormSubmit}
+        >
             <fieldset className="form__container">
                 <legend className="form__description">Kalkulator walutowy</legend>
                 <p className="forn__paragraph">
-                    <label className="form__item"><span className="form__ItemDescription">Podaj kwotę w PLN: </span>
-                        <input type="number" min="1" step="1" placeholder="Wpisz kwotę..." className="form__input js-pln"
-                            name="pln" />
+                    <label className="form__item">
+                        <span className="form__ItemDescription">Podaj kwotę w PLN: </span>
+                        <input
+                            type="number"
+                            min="1" step="1"
+                            placeholder="Wpisz kwotę..."
+                            className="form__input "
+                            name="pln"
+                            value={amount}
+                            onChange={onInputChange}
+                        />
                     </label>
                 </p>
                 <label className="form__item"> <span className="form__ItemDescription">Wybierz walutę</span>
-                    <select className="form__input js-currency" name="currency">
-                        <option value="EUR">EUR</option>
-                        <option value="USD">USD</option>
-                        <option value="CHF">CHF</option>
+                    <select
+                        value={currency}
+                        onChange={onSelectChange}
+                        className="form__input"
+                        >
+                        <option value={eur}>EUR</option>
+                        <option value={usd}>USD</option>
+                        <option value={chf}>CHF</option>
                     </select>
                 </label>
 
             </fieldset>
-            <button className="form__button js-button">Przelicz</button>
-            <button type="reset" className="form__button js-buttonReset">Wyczyść formularz</button>
+            <button className="form__button">Przelicz</button>
+            <button className="form__button">Wyczyść formularz</button>
         </form>
     )
 }
