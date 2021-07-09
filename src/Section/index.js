04e7df398c8f38ecf755f3f1ad16e5c3b2872518
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-07-09 21:15:50 +0200

    result display interaction

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,11 +1,11 @@
 import "./style.css";
 
-const Section = () => (
+const Section = (props) =>(
     <section className="section">
         <span className="section__description">Wynik to: </span>
-        <strong className="section__result "></strong>
+        <strong className="section__result">{props.result}</strong>
     </section>
-
-);
+)
+    ;
 
 export default Section;
