d1d59b2f3dcd49dee48ffc75a50f092c29acba4f
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-07-25 17:24:10 +0200

    conditional functions for section__result added

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,19 +1,21 @@
 import "./style.css";
 
 export const Section = ({ result }) => (
-    <section className="section">
-       <p className="section__body">
-       <span className="section__description">Wynik to: </span>
-        <strong className="section__result">
-            {result !== undefined && (
-                <>
-                    {result.sourceAmount.toFixed(2)}{" "}PLN{" "}={" "}
-                    {result.targetAmount.toFixed(2)}{" "}{result.currency}
-                </>
-            )}
-        </strong>
-       </p>
-    </section>
-)
-    ;
+  <section className="section">
+    <p className="section__body">
+      <span className="section__description">Wynik to: </span>
+      <strong className="section__result">
+        {result && (
+          <>
+            {result.sourceAmount
+              ? ` ${result.sourceAmount.toFixed(2)} PLN =`
+              : ""}
+            {result.targetAmount ? result.targetAmount.toFixed(2) : ""}
+            {result.sourceAmount ? result.currency : ""}
+          </>
+        )}
+      </strong>
+    </p>
+  </section>
+);
 
