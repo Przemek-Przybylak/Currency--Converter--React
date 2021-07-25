1641291896c957a8dd7e0c25918e1e59acd514f9
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-07-25 17:17:30 +0200

    transfer getResult to Form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import { useState } from 'react';
 import { Form } from './Form';
-import { currencies } from './Form/currencies';
 import { Container } from "./Container";
 
-function App() {
-  const [result, setResult] = useState();
-
-  const getResult = (currency, amount) => {
-    if (amount <= 0)
-    return "";
-    
-    const rate = currencies.find(
-      ({ name }) => name === currency).rate
-
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount / rate,
-      currency
-    })
-  };
+function App(result, getResult) {
 
   return (
     <Container >
@@ -27,6 +10,6 @@ function App() {
         getResult={getResult} />
     </ Container>
   );
-}
+};
 
 export default App;
