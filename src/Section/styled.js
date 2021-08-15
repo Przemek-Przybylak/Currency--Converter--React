ee34c6080dcf1c70b0d831d7d405dfddda8ac121
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-08-15 11:33:11 +0200

    create styled for section result

---
+++ src/Section/styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const StyledSection = styled.section`
+    margin: 0 auto;
+    width: 100%;
+    text-align: center;
+    max-width: 300px;
+    background-color: rgb(4, 164, 228);
+    color: white;
+    font-weight: 700;
+    padding: 5px;
+`;
+
+export const StyledParagraph = styled.p`
+    padding: 0;
+    margin: 0;
+`;
