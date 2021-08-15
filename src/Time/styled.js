c5713ea059919f44c60818c9f812c068d0350d9b
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-08-15 11:41:57 +0200

    create styled components for Time component

---
+++ src/Time/styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledDiv = styled.div`
+    margin: 0px;
+    margin-bottom: 10px;
+    color: rgb(172, 172, 172);
+    font-family: 'Roboto Mono', monospace;
+    font-size: 10px;
+`;
+
+export const StyledParagraph = styled.p`
+    margin: 0px;
+    display: flex;
+    justify-content: flex-end;
+`;
