11dd881e9da427f5e16937c43a64075b9479aad8
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-08-15 11:23:17 +0200

    create styled for Container

---
+++ src/Container/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.main`
+    margin: 0px auto;
+    background-image: url("https://cdn.wallpapersafari.com/14/53/1xAB3U.jpg");
+    background-size: cover;
+    height: 100vh;
+    overflow-y: hidden;
+`;
