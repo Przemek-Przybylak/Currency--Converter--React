1e298c5ab80018c3a840accabf5b8586be3349b0
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-09-20 14:32:13 +0200

    refactoring, added useCuurrentData hook in Form

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,26 +2,34 @@ import { useState } from "react";
 import { currencies } from "./currencies";
 import { Section } from "../Section";
 import { Time } from "../Time";
-import { StyledForm, StyledDiv, StyledLabel, StyledFieldset, StyledLegend, StyledSpan, StyledInput, StyledButton } from "./styled";
+import { useCurrentData } from "./useCurrentData";
+import {
+    StyledForm,
+    StyledDiv,
+    StyledLabel,
+    StyledFieldset,
+    StyledLegend,
+    StyledSpan,
+    StyledInput,
+    StyledButton
+} from "./styled";
 
 export const Form = () => {
-    const [result, setResult] = useState("");
-    const [currency, setCurrency] = useState(currencies[0].name);
-    const [amount, setAmount] = useState("");
+    const [result, setResult] = useState();
+    const ratesData = useCurrentData();
 
     const getResult = (currency, amount) => {
-        if (amount <= 0)
-            return "";
-
-        const rate = currencies.find(
-            ({ name }) => name === currency).rate
+        const rate = ratesData.rates[currency];
 
         setResult({
             sourceAmount: +amount,
-            targetAmount: amount / rate,
-            currency
-        })
+            targetAmount: amount * rate,
+            currency,
+        });
     };
+
+    const [currency, setCurrency] = useState("EUR");
+    const [amount, setAmount] = useState("");
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -38,36 +46,49 @@ export const Form = () => {
             <StyledDiv>
                 <Time />
                 <StyledFieldset>
-                    <StyledLegend>Kalkulator walutowy</StyledLegend>
-                    <p>
-                        <StyledLabel>
-                            <StyledSpan>Podaj kwotę w PLN: </StyledSpan>
-                            <StyledInput
-                                type="number"
-                                min="1" step="1"
-                                placeholder="Wpisz kwotę..."
-                                className="form__input "
-                                value={amount}
-                                onChange={({ target }) => setAmount(target.value)}
-                            />
-                        </StyledLabel>
-                    </p>
-                    <StyledLabel>
-                        <StyledSpan>Wybierz walutę</StyledSpan>
-                        <StyledInput as="select"
-                            value={currency}
-                            onChange={({ target }) => setCurrency(target.value)}
-                        >
-                            {currencies.map((currency => (
-                                <option
-                                    key={currency.name}
-                                    value={currency.name}
+                    {ratesData.state === "loading" ? (
+                        <p>
+                            Proszę czekać, trwa pobieranie danych...
+                        </p>
+                    ) : ratesData.state === "error" ? (
+                        <p>
+                            Niestety nie udało się załadować walut<br />
+                            Sprawdz czy masz internet, jeśli masz to spróbuj później!
+                        </p>
+                    ) : (
+                        <>
+                            <StyledLegend>Kalkulator walutowy</StyledLegend>
+                            <p>
+                                <StyledLabel>
+                                    {currencies}
+                                    <StyledSpan>Podaj kwotę w PLN: </StyledSpan>
+                                    <StyledInput
+                                        type="number"
+                                        min="1" step="1"
+                                        placeholder="Wpisz kwotę..."
+                                        className="form__input "
+                                        value={amount}
+                                        onChange={({ target }) => setAmount(target.value)}
+                                    />
+                                </StyledLabel>
+                            </p>
+                            <StyledLabel>
+                                <StyledSpan>Wybierz walutę</StyledSpan>
+                                <StyledInput as="select"
+                                    value={currency}
+                                    onChange={({ target }) => setCurrency(target.value)}
                                 >
-                                    {currency.name}
-                                </option>
-                            )))};
-                        </StyledInput>
-                    </StyledLabel>
+                                    {Object.keys(ratesData.rates).map((currency => (
+                                        <option
+                                            key={currency}
+                                            value={currency}
+                                        >
+                                            {currency}
+                                        </option>
+                                    )))};
+                                </StyledInput>
+                            </StyledLabel>
+                        </>)}
                 </StyledFieldset>
                 <StyledButton onClick={onFormSubmit}>Przelicz</StyledButton>
                 <StyledButton type="reset" onClick={onClickClear} >Wyczyść formularz</StyledButton>
