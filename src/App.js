8527cfdbef7f1d12b262c660bf917fd624368f9d
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-09-20 14:30:19 +0200

    added useCurrentData in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Form } from './Form';
 import { Container } from "./Container";
+import { useCurrentData } from './Form/useCurrentData';
 
 function App(result, getResult) {
+  const currenciess = useCurrentData();
 
   return (
     <Container >
       <Form
         result={result}
+        currenciess={currenciess}
         getResult={getResult} />
     </ Container>
   );
